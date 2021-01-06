b52476cc4f826c1f8a08ba8b503de68b02222849
Mateus <mateuswagnitzdev@gmail.com>
2021-01-05 21:41:07 -0300

    Add project files.

---
+++ EFcore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,25 @@
+﻿using EFcore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFcore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+
+
+        public HeroiContext(DbContextOptions<HeroiContext> options) : base(options) { }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseMySqlServer("");
+        //}
+    }
+
+}
